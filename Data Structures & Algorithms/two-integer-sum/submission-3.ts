24237de636cb2505914d5ed0f2603cc0d87584ea
Luke Floden <luke.floden@gmail.com>
2026-06-09 12:48:20 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.ts
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = new Map<number, number>();
+
+        for (let i=0; i < nums.length; i++){
+            const current = nums[i];
+            const compliment = target - current;
+            if (map.has(compliment)) {
+                return [i, map.get(compliment)]
+            }
+            map.set(current, i);
+        }
+
+        throw new Error('NOT FOUND')
+    }
+
+    twoSumSorted(nums: number[], target: number): number[] {
+        // 2. two pointers approach
+        let front = 0, back = nums.length - 1;
+        //console.log(front, back)
+        for (let i=0; i < nums.length; i++){
+            const current = nums[front] + nums[back];
+            //console.log('f,b,current: ', nums[front], nums[back], current)
+
+            if (current > target) {
+                back--
+            } else if (current < target) {
+                front ++
+            } else {
+                return [front, back]
+            }
+        }
+        throw new Error('NOT FOUND')
+    }
+}
+
