fde7481098e409437baf6e5a8fc6edab0cb0b98e
Luke Floden <luke.floden@gmail.com>
2026-06-19 12:54:24 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        //s = s.toLowerCase().replace(/[\s\p{P}]+/gu, "");
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+        let i = 0,
+            j = s.length - 1;
+        while (i < j) if (s[i++] !== s[j--]) return false;
+        return true;
+
+        // const punc = new Set('.?!,;-')
+        // for (let i=0, j=s.length-1; i<Math.floor(s.length/2); i++, j--){
+        //     let l = s[i];
+        //     let r = s[j];
+        //     // while (punc.has(l)) l = s[++i]
+        //     // while (punc.has(r)) r = s[--j]
+        //     // console.log('<><><>')
+        //     // console.log(l,r)
+        //     if (l !== r)
+        //         return false
+        // }
+        // return true
+    }
+}
